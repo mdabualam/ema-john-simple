b6e51496f518ae09192b8bae668f00a4ddfe0112
Md Abu Alam <abualam@outlook.com>
2020-09-02 02:47:00 +0600

    modified

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Cart = (props) => {
     const cart = props.cart;
-    console.log(cart);
+    // console.log(cart);
     // const total = cart.reduce((total, prd) => total + prd.price, 0);
-
     let total =0;
     for (let i = 0; i < cart.length; i++) {
         const product = cart[i];
-        total = total + product.price;
+        total = total + product.price * product.quantity;
     }
     let shipping = 0;
     if (total > 35) {
@@ -37,6 +37,10 @@ const Cart = (props) => {
             <p><small>Shipping Cost: {shipping}  </small></p>
             <p><small>Tax + VAT: {tax} </small></p>
             <p>Total Price: {grandTotal}</p>
+            <br/>
+            <Link to="/review">
+            <button className="main-button">Review Order</button>
+            </Link>
         </div>
     );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
            <nav>
                <a href="/shop">Shop</a>
                <a href="/review">Review</a>
-               <a href="/manage">Manage Inventory here</a>
+               <a href="/inventory">Manage Inventory here</a>
            </nav>
         </div>
     );
